259482e00ae24c19fdb4512856096f35cf34c43e
Gabriel Reis Andrade <joaogabriel2@gmail.com>
2021-07-15 22:36:59 -0300

    Adjusts to the repel.

--- Assets/Scripts/Repel.cs
+++ Assets/Scripts/Repel.cs
@@ -5,25 +5,18 @@ using UnityEngine;
 public class Repel : MonoBehaviour
 {
     public GameObject ball;
-    private Rigidbody ballRigidbody;
     public float forceOnImpact = 100;
-    private void Start()
-    {
-        ballRigidbody = ball.GetComponent<Rigidbody>();
-    }
     private void OnCollisionEnter(Collision collision)
     {
         if(collision.gameObject.name == ball.name)
         {
-            StartCoroutine(Expand(collision.gameObject, collision.contacts[0].normal));
+            StartCoroutine(Expand(collision.gameObject.GetComponent<Rigidbody>(), collision.contacts[0].normal));
         }
     }
-    IEnumerator Expand(GameObject b, Vector3 normal)
+    IEnumerator Expand(Rigidbody b, Vector3 normal)
     {
-        //Vector3 vector3 = new Vector3(-ball.transform.position.x * forceOnImpact, ball.transform.position.y, -ball.transform.position.z * forceOnImpact);
         transform.localScale = new Vector3(1.2f, 1, 1.2f);
-        b.GetComponent<Rigidbody>().AddForce(normal*forceOnImpact);
-        //ballRigidbody.velocity = Vector3.Reflect(ballRigidbody.velocity, normal);
+        b.GetComponent<Rigidbody>().AddForce(-normal*forceOnImpact);
         yield return new WaitForSeconds(0.5f);
         transform.localScale = Vector3.one;
     }
