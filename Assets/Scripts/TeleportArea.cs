a5dedd92af62745fe6abb704412e93b9e2b394b6
Gabriel Reis Andrade <joaogabriel2@gmail.com>
2021-07-19 12:20:54 -0300

    Added teleportation.

---
+++ Assets/Scripts/TeleportArea.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TeleportArea : MonoBehaviour
+{
+    public GameObject ball;
+    public GameObject teleportation;
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.name == ball.name && teleportation.GetComponent<Teleportation>().inUse == false)
+        {
+            StartCoroutine(teleportation.GetComponent<Teleportation>().Teleport(this.gameObject));
+        }
+    }
+}
---
+++ Assets/Scripts/Teleportation.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Teleportation : MonoBehaviour
+{
+    public GameObject ball; // needs to be the instance of the ball and not its prefab.
+    public GameObject teleporterArea1;
+    public GameObject teleporterArea2;
+    public Vector3 teleportationLocation1;
+    public Vector3 teleportationLocation2;
+    public bool inUse = false;
+    public IEnumerator Teleport(GameObject originGameObject)
+    {
+        inUse = true;
+        if (originGameObject == teleporterArea1)
+        {
+            ball.transform.position = teleportationLocation2;
+        }
+        else if(originGameObject == teleporterArea2)
+        {
+            ball.transform.position = teleportationLocation1;
+        }
+        yield return new WaitForSeconds(1);
+        inUse = false;
+    }
+}
