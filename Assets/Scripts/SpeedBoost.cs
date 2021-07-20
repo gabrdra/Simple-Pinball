55b0bb396eb29d859efa68c27ec552e6e49c3317
Gabriel Reis Andrade <joaogabriel2@gmail.com>
2021-07-19 22:55:51 -0300

    Adjusts to the board and SpeedBoost obstacle.

---
+++ Assets/Scripts/SpeedBoost.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpeedBoost : MonoBehaviour
+{
+    public GameObject ball;
+    public float boostForce = 300;
+    public Vector3 directionOfBoost = Vector3.forward;
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.name == ball.name)
+        {
+            collision.gameObject.GetComponent<Rigidbody>().AddForce(directionOfBoost * boostForce);
+        }
+    }
+}
